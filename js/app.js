2b416a758662c99238b76226856a7cc7c0d6cb4a
JoaoM10 <joao_m-4@hotmail.com>
2014-11-04 17:32:39 +0000

    init grid to config

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 (function() {
   var app = angular.module('Senkan', []);
+  gameMode = 1;
 
   app.controller('TabController', function(){
     this.tab = 1;
 
     this.setTab = function(newValue){
       this.tab = newValue;
+      gameMode = 1;
     };
 
     this.isSet = function(tabName){
@@ -13,7 +15,33 @@
     };
   });
 
-  
+  app.controller('GameController', function(){
 
+    this.setMode = function(newValue){
+      gameMode = newValue;
+      if(gameMode === 2)
+        init_game_board();
+    };
+
+    this.isSet = function(modeName){
+      return gameMode === modeName;
+    };
+  });
+
+
+  function init_game_board() {
+    $("#wrapper").empty();
+
+    $("#wrapper").append("<div class=\"square square-corner\"></div>");
+    for(i = 1; i <= 10; i ++)
+      $("#wrapper").append("<div class=\"square square-head\">" + i.toString() + "</div>");
+    for(i = 0; i < 10; i ++){
+      $("#wrapper").append("<div class=\"square square-head\">" + String.fromCharCode("A".charCodeAt(0) + i) + "</div>");
+      for(j = 0; j < 10; j ++){
+        $("#wrapper").append("<div class=\"square\"></div>");
+      }
+    }
+
+  }
 
 })();
