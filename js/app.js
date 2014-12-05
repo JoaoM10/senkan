adc60ba23b1d5558b17a20d4430691e6e94cbc8c
JoaoM10 <joao_m-4@hotmail.com>
2014-12-05 20:02:00 +0000

    hall of fame live

--- js/app.js
+++ js/app.js
@@ -9,28 +9,12 @@
       this.tab = newValue;
       gameMode = 1;
       if(this.tab === 3)
-        $('#hof-update').click();
+        hof_update();
     };
 
     this.isSet = function(tabName){
       return this.tab === tabName;
     };
-  });
-
-  app.controller('HallOfFameController', function(){
-    this.hall_of_fame = [
-      {pos: 1, username: 'Joao', score: 83},
-      {pos: 2, username: 'Filipe', score: 82},
-      {pos: 3, username: 'Pedro', score: 81},
-      {pos: 4, username: 'Joaquim', score: 80},
-      {pos: 5, username: 'Miguel', score: 79},
-      {pos: 6, username: 'Antonio', score: 78},
-      {pos: 7, username: 'Rita', score: 77},
-      {pos: 8, username: 'Maria', score: 76},
-      {pos: 9, username: 'Andre', score: 75},
-      {pos: 10, username: 'Ze', score: 74}
-    ];
-
   });
 
   app.controller('GameController', function(){
---
+++ js/hof.js
@@ -0,0 +1,34 @@
+
+function hof_update(){
+
+  var req = new XMLHttpRequest();
+  req.open("post", "http://twserver.alunos.dcc.fc.up.pt:8000/ranking", true);
+  req.onreadystatechange = function(){
+    if(req.readyState != 4){ return; }
+    if(req.status != 200){ 
+      alert("There was an error updating the hall of fame!");
+      return;
+    }
+    var hof = JSON.parse(req.responseText);
+
+    $('#hof-body').html('');
+    for(var i = 0; i < 10; i ++){
+      var tr = document.createElement('tr');
+    
+      var td_px = document.createElement('td');
+      td_px.appendChild(document.createTextNode(i + 1));
+      tr.appendChild(td_px);
+    
+      var td_un = document.createElement('td');
+      td_un.appendChild(document.createTextNode(hof.ranking[i].name));
+      tr.appendChild(td_un);
+    
+      var td_pt = document.createElement('td');
+      td_pt.appendChild(document.createTextNode(hof.ranking[i].shots));
+      tr.appendChild(td_pt);
+
+      $('#hof-body').append(tr);
+    }
+  }
+  req.send("{}");
+}
